512cc2c6b245114af4506aea6527e514d0440b4f
poormi <jingdai8@gmail.com>
2018-04-11 17:33:05 +0800

    modify filter style

--- src/components/FilterItems.vue
+++ src/components/FilterItems.vue
@@ -1,14 +1,14 @@
 <template>
 	<div class="filters">
-		<h3 class="f-title">{{title}}</h3>
+		<h3 class="f-title">{{title}} <span v-if="multiSelect" class="s-title">(可多选)</span></h3>
 		<ul class="filter-ul" :class="'len-'+ data.length">
-			<li v-if="expanded  true : index <= 7" class="filter-item" v-for="(item,index) in data" :class="cssClass(item, index)" @click="selectItem(item,index)">
-				<label class="shadow check item" v-if="multiSelect" :for="'chk_'+item.key">
+			<li v-if="expanded?true:index<=7" class="filter-item" v-for="(item,index) in data" :class="cssClass(item, index)" @click="selectItem(item,index)">
+				<label class="shadow item" v-if="multiSelect" :for="'chk_'+item.key">
 					<input :id="'chk_'+item.key" type="checkbox" :value="item" v-model="value">
-					{{item[name]}}
+					{{item[name]|setAll}}
 				</label>
-				<span class="shadow item" :class="{'more': index == 7 && !expanded}" v-else-if="index == 7 && !expanded">...</span>
-				<span class="shadow item" v-else>{{item[name]}}</span>
+				<span class="shadow item" :class="{'more': index == 7 && !expanded}" v-else-if="index==7&&!expanded">...</span>
+				<span class="shadow item" v-else>{{item[code]=='ALL'?'全部':item[name]|setAll}}</span>
 			</li>
 		</ul>
 	</div>
@@ -24,6 +24,11 @@
 			return {
 				value: v,
 				expanded: false
+			}
+		},
+		filters: {
+			setAll(name){
+				return name.indexOf('全部') !== -1 ? '全部' : name
 			}
 		},
 		props:{
@@ -96,7 +101,7 @@
 					if(d.hasOwnProperty('key')){ // this filter item is exclude 
 						return key.indexOf(d.key) !== -1
 					}else {
-						const isSelected = d[code] == value
+						const isSelected = d[code] == value || (value === '99999' && d[code]==='ALL')
 						selectedIndex = isSelected && i // index for expanded the list
 						return isSelected
 					}
@@ -120,28 +125,35 @@
 
 <style scope lang="scss">
 	.filters{
-		padding: 30px 18px;
+		padding: 10px 25px;
 		color: #505050;
 	}
 	.f-title{
-		margin: 12px 0;
+		margin: 0px 0;
 		font-size: 24px;
 		font-weight: normal;
+	}
+	.s-title{
+		font-size: 20px;
+		color: #969696;
 	}
 	.filter-ul{
 		display: flex;
 		flex-flow: row wrap;
 	}
 	.filter-item{
-		margin: 5px 0;
-		width: 25%;
+		margin: 9px 0;
+		width: 33%;
 		font-size: 24px;
-		text-align: center;
 		.item{
 			display: inline-block;
-			padding: 20px 0;
+			padding: 20px 0;			
+			border: solid 1px transparent;/*no*/
 			border-radius: 3px;/*no*/
-			width: 170px;
+			min-width: 170px;
+			height: 73px;
+			line-height: 1.2;
+			text-align: center;
 			&.check{
 				width: 266px;
 			}
@@ -150,6 +162,7 @@
 				height: 73px;
 				line-height: 0;
 				font-size: 60px;
+				color: #969696;
 			}
 		}
 		&.multi{
@@ -160,9 +173,20 @@
 			margin-left: 0;
 		}
 		&.on .shadow{
-			background-color: #53d3ff;
-			color: #fff;
+			position: relative;
+			color: #53d3ff;
+			border: solid 1px #53d3ff;/*no*/
 			box-shadow: none;
+			&:after{
+				content: '';
+			    position: absolute;
+			    bottom: 0;
+			    right: 0;
+			    width: 30px;
+			    height: 30px;
+			    background: url(../../assets/images/icon-selected.png) right bottom no-repeat;
+			    background-size: 100% 100%;
+			}
 		}
 		input[type=checkbox]{
 			display: none;
