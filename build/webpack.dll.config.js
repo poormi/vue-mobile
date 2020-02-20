fe441c0385ec5c7b30b2da718379690609b4a73b
jing <jingdai8@gmail.com>
2020-02-20 15:20:36 +0800

    Create webpack.dll.config.js

---
+++ build/webpack.dll.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const path = require('path');
+const vendors - [
+  'vue',
+  'vue-router',
+  'vuex',
+  'lodash',
+  'moment',
+  'axios',
+  'zrender',
+  'echarts'
+];
+ 
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, '../dll'),
+    filename: '[name].[chunkhash].js',
+    library: '[name]_[chunkhash]'
+  },
+  entry: {
+    vendor: vendors
+  },
+  plugins: [
+    new webpack.DllPlugin({
+      path: path.resolve('./dll', 'manifest.json'),
+      name: '[name]_[chunkhash]'
+    })
+  ]
+}
